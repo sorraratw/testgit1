835e74ee4fba586af00fd4bb297507673ec07499
sorrarat <sorrarat.ouu@gmail.com>
2022-12-07 13:59:24 +0700

    new commit

---
+++ test.py
@@ -0,0 +1,8 @@
+# echo "# testgit1" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/sorraratw/testgit1.git
+# git push -u origin main
+print("test git")
